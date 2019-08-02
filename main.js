24873d92bf3cbe020fea3619504961e65339b493
Sameehan Patel <sp4315@nyu.edu>
2019-08-01 21:37:08 -0400

    Stella's got her groove back

--- main.js
+++ main.js
@@ -6,16 +6,27 @@ class App extends Component {
         super()
         this.state = {
             users: (new Array(20)).fill().map(user => {
-                return faker.helpers.createCard()
+                user = faker.helpers.createCard()
+                user.isFavorite = false 
+                return user
             })
         }
     }
+
     render() {
         const users = this.state.users
         console.log(users)
-        const usersElements = users.map(user => React.createElement('h1', {key: user.username}, user.name, user.username))
+        const clickEvent = (ev) => {
+        let idx = ev.target.dataset.index
+        users[idx].isFavorite = !users[idx].isFavorite;
+        this.setState({users});
+
+        }
+        const usersElements = users.map((user, idx) => React.createElement('div', {key: user.username, "data-index": idx, className:user.isFavorite ? "favorite" : "",  onClick: (ev) => clickEvent(ev)}, user.name, user.username))
         return usersElements
     }
 }
 
 ReactDOM.render(React.createElement(App), root)
+
+
